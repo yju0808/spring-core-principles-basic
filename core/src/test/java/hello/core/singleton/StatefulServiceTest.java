854339091831d7b6ad58532da3274de14003f692
Kant Lim <yju0808@naver.com>
2025-04-28 14:08:01 +0000

    feat: singleton example

---
+++ core/src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -0,0 +1,41 @@
+package hello.core.singleton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+public class StatefulServiceTest {
+
+
+
+    public static class TestConfig {
+
+        @Bean
+        public StatefulService statefulService() {
+            return new StatefulService();
+        }
+    }
+
+    @Test
+    void statefulServiceSingleton() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+
+        StatefulService statefulService1 = ac.getBean(StatefulService.class);
+        StatefulService statefulService2 = ac.getBean(StatefulService.class);
+
+        // ThreadA: A사용자 10000원 주문
+        statefulService1.order("userA", 10000);
+
+        // ThreadB: B사용자 20000원 주문
+        statefulService2.order("userB", 20000);
+
+        // ThreadA: 사용자 A 주문 금액 조회
+        int price = statefulService1.getPrice();
+        System.out.println("price = " + price);
+        // 기대한 결과는 10000원이지만, 20000원이 나옴
+
+
+        
+    }
+}
